daecc96d81cfd654d43b8421076ebdb0417abb80
gustavocnp95 <gustavocnavarro@gmail.com>
2022-05-29 21:59:49 -0300

    inicia implementação da tela de visualização de usuário

--- lib/user/infos/user_visualization_edition_page.dart
+++ lib/user/infos/user_visualization_edition_page.dart
@@ -1,17 +1,71 @@
 import 'package:caldo_cana_campeao/custom_widgets/campeao_app_bar.dart';
 import 'package:flutter/material.dart';
 
+import '../../color/theme_colors.dart';
+import '../../images/images.dart';
+
 class UserVisualizationEditionPage extends StatefulWidget {
+  const UserVisualizationEditionPage({Key? key}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() => _UserVisualizationEditionPageState();
 }
 
 class _UserVisualizationEditionPageState
     extends State<UserVisualizationEditionPage> {
+  String _screenTitle = "Visualizar cadastro";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: CampeaoAppBar(),
+      appBar: const CampeaoAppBar(),
+      body: Padding(
+        padding: EdgeInsets.only(left: 8, right: 8),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 10,
+                      color: Colors.black38,
+                      spreadRadius: 1,
+                    )
+                  ],
+                ),
+                child: CircleAvatar(
+                  backgroundColor: CampeaoColors.primaryColor,
+                  radius: 100,
+                  child: Padding(
+                    padding: const EdgeInsets.all(4),
+                    child: ClipRRect(
+                      child: Image.asset(Images.caldoCanaCampeaoLogo),
+                      borderRadius: BorderRadius.circular(1000),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 56),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  _screenTitle,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w700,
+                      color: CampeaoColors.primaryTextColor,
+                      fontSize: 25),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
