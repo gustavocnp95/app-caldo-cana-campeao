ef12f00503547abbdaa515e181c3dbff0279b102
gustavocnp95 <gustavocnavarro@gmail.com>
2022-05-29 21:37:39 -0300

    altera paleta de cores

--- lib/color/theme_colors.dart
+++ lib/color/theme_colors.dart
@@ -1,9 +1,9 @@
 import 'dart:ui';
 
 class CampeaoColors {
-  static const Color primaryColor = Color.fromRGBO(238, 160, 62, 1);
-  static const Color primaryColorDark = Color.fromRGBO(240, 125, 2, 1);
-  static const Color primaryColorSoft = Color.fromRGBO(242, 124, 4, 0.16);
+  static const Color primaryColor = Color.fromRGBO(153, 188, 0, 1);
+  static const Color primaryColorDark = Color.fromRGBO(116, 138, 19, 1);
+  static const Color primaryColorSoft = Color.fromRGBO(165, 214, 167, 0.25);
   static const Color primaryTextColor = Color.fromRGBO(69, 69, 69, 1);
-  static const Color primaryBackgroundColorSoft = Color.fromRGBO(253, 234, 215, 1);
+  static const Color primaryBackgroundColorSoft = Color.fromRGBO(232, 245, 233, 1);
 }
